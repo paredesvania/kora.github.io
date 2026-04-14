9655bfff52e1166211d1ba6c797d5639a495015c
vania;) <vania.paredes@mail.udp.cl>
2026-04-14 19:56:06 -0400

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -6,3 +6,15 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.left = e.clientX + "px";
   cursor.style.top = e.clientY + "px";
 });
+
+const links = document.querySelectorAll("a");
+
+links.forEach(link => {
+  link.addEventListener("mouseenter", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1.8)";
+  });
+
+  link.addEventListener("mouseleave", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1)";
+  });
+});
