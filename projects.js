93e1381d25292532a2afa957834868e05a74ae12
vania;) <vania.paredes@mail.udp.cl>
2026-04-14 17:24:01 -0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,33 +1,40 @@
+const projects = {
+  albura: {
+    title: "Albura",
+    content: `
+      <img src="assets/albura/imagen1.jpg" style="width:100%">
+      <p>Exploración en madera basada en ensamblaje modular.</p>
+    `
+  },
+
+  feria: {
+    title: "Feria móvil",
+    content: `
+      <img src="assets/feria-movil/imagen1.jpg" style="width:100%">
+      <p>Sistema urbano adaptable para comercio.</p>
+    `
+  },
+
+  lemontech: {
+    title: "Lemontech",
+    content: `
+      <img src="assets/lemontech/imagen1.jpg" style="width:100%">
+      <p>Identidad visual tecnológica.</p>
+    `
+  }
+};
+
 function openProject(id) {
   const windowEl = document.getElementById('project-window');
   const content = document.getElementById('window-content');
   const title = document.getElementById('window-title');
 
+  const project = projects[id];
+
+  title.innerText = project.title;
+  content.innerHTML = project.content;
+
   windowEl.classList.remove('hidden');
-
-  if (id === 'albura') {
-    title.innerText = 'Albura';
-    content.innerHTML = `
-      <img src="assets/albura/imagen1.jpg" style="width:100%">
-      <p>Exploración en madera basada en ensamblaje modular.</p>
-    `;
-  }
-
-  if (id === 'feria') {
-    title.innerText = 'Feria móvil';
-    content.innerHTML = `
-      <img src="assets/feria-movil/imagen1.jpg" style="width:100%">
-      <p>Sistema urbano adaptable para comercio.</p>
-    `;
-  }
-
-  if (id === 'lemontech') {
-    title.innerText = 'Lemontech';
-    content.innerHTML = `
-      <img src="assets/lemontech/imagen1.jpg" style="width:100%">
-      <p>Identidad visual tecnológica.</p>
-    `;
-  }
 }
 
 function closeProject() {
