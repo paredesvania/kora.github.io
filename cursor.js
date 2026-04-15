6869c58439f198ac52555f4ad0475dd92f69133b
vania;) <vania.paredes@mail.udp.cl>
2026-04-15 13:10:52 -0400

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -14,37 +14,3 @@ link.addEventListener("mouseleave", () => {
 cursor.style.transform = "translate(-50%, -50%) scale(1)";
 });
 });
-
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector("nav");
-  if (window.scrollY > 10) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-  }
-});
-document.addEventListener("DOMContentLoaded", () => {
-  const transition = document.getElementById("page-transition");
-
-  // entrada
-  if (transition) {
-    transition.style.transform = "scaleY(0)";
-  }
-
-  // salida en links
-  document.querySelectorAll("a").forEach(link => {
-    link.addEventListener("click", (e) => {
-      const href = link.getAttribute("href");
-
-      if (!href.includes("#") && !href.includes("mailto")) {
-        e.preventDefault();
-
-        transition.style.transform = "scaleY(1)";
-
-        setTimeout(() => {
-          window.location = href;
-        }, 450);
-      }
-    });
-  });
-});
