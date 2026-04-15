877a4a8b8cad0b5cfb1b392c283d8964129fd59a
vania;) <vania.paredes@mail.udp.cl>
2026-04-15 11:27:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,28 +54,6 @@ document.addEventListener("DOMContentLoaded", () => {
   type();
 });
 
-let points = [];
-
-function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  canvas.style('z-index', '-2');
-}
-
-function draw() {
-  background(250); // var(--white)
-  
-  // Dibujar grid de círculos que reaccionan al mouse
-  for (let x = 0; x < width; x += 40) {
-    for (let y = 0; y < height; y += 40) {
-      let d = dist(mouseX, mouseY, x, y);
-      let size = map(d, 0, 300, 15, 2, true);
-      fill(d < 100 ? '#ff2aa3' : 200);
-      noStroke();
-      ellipse(x, y, size, size);
-    }
-  }
-}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
