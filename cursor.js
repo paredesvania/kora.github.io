e313a32b4a15d4d1e6b34c1c378caeac4ccc8ac4
vania;) <vania.paredes@mail.udp.cl>
2026-04-15 11:30:36 -0400

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -3,30 +3,16 @@ cursor.classList.add("cursor");
 document.body.appendChild(cursor);
 
 document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
 });
 
 const links = document.querySelectorAll("a");
-
 links.forEach(link => {
-  link.addEventListener("mouseenter", () => {
-    cursor.style.transform = "translate(-50%, -50%) scale(1.8)";
-  });
-
-  link.addEventListener("mouseleave", () => {
-    cursor.style.transform = "translate(-50%, -50%) scale(1)";
-  });
-});
-document.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', e => {
-    if (link.hostname === window.location.hostname) {
-      e.preventDefault();
-      const target = link.href;
-      document.querySelector('.page-transition').classList.add('active');
-      setTimeout(() => {
-        window.location.href = target;
-      }, 600);
-    }
-  });
+    link.addEventListener("mouseenter", () => {
+        cursor.style.transform = "translate(-50%, -50%) scale(1.8)";
+    });
+    link.addEventListener("mouseleave", () => {
+        cursor.style.transform = "translate(-50%, -50%) scale(1)";
+    });
 });
